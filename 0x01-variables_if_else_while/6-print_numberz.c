e890377b627c5e5e5f9bc03dddd2263713a6c4f3
ifefola <olagokeifeoluwa@gmail.com>
2022-10-18 13:31:17 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single integer from 0-10
+ *
+ */
+
+int main(void)
+
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+	
